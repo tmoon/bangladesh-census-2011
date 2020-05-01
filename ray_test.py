b4e950431735bf429ec98ae127459308310e52ce
Tarik Moon <tarik@tarikmoon.com>
2020-05-01 14:37:23 -0700

    dist remote

--- ray_test.py
+++ ray_test.py
@@ -7,48 +7,47 @@ import os
 
 print("PID", os.getpid())
 ray.init()
-
-
-# @ray.remote
-# class Counter(object):
-#     def __init__(self):
-#         self.value = 0
-
-#     def increment(self):
-#         self.value += 1
-#         return self.value
-
         
-# @ray.remote
+@ray.remote
+class District(object):
+    """docstring for District"""
+    def __init__(self, id):
+        self.id = id
+        self.persons_dict = {}
+    
+    def register_person(self, person):
+        self.persons_dict[person.uid] = person
+    
+
+    def num_members(self):
+        return len(self.persons_dict)
+
+
 class Person(object):
     """docstring for Person"""
-    def __init__(self, id):
-        # super(Person, self).__init__()
-        self.id = id
-        self.arr = np.random.rand(1000)
-    
-    def get_arr(self, i):
-        return self.arr[i]
-
+    def __init__(self, uid, features = None):
+        self.uid = uid
+        self.features = np.random.rand(100) if features is None else features
+        
 if __name__ == '__main__':
     p_arr = []
-    p_val_arr = []
     M = 1000000
 
-    # for i in range(10 * M):
-    #     p = Person(i)
-    #     p_val_arr.append(p.arr[i % 100])
-    #     if i % M == 1:
-    #         print(i, " ")
+    NUM_DIST = 4
 
-    # del(p_val_arr)
-    # # Create ten < Counter actors.
-    counters = [Person(id) for id in range(1000)]
+    dists = [District.remote(i) for i in range(NUM_DIST)]
 
-    # # Increment each Counter once and get the results. These tasks all happen in
-    # # parallel.
-    results = ray.get([c.get_arr.remote(13) for c in counters])
+    for i in range(M):
+        p = Person(i)
 
-    # print(results)  # prints [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        dists[i % NUM_DIST].register_person.remote(p)
+
+        if i % M == 1:
+            print(i, " ")
+
+    results = ray.get([d.num_members.remote() for d in dists])
+
+
+    print(results)
     print("DONE")
     time.sleep(100)
